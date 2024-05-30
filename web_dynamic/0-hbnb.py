a930290c53a770055d508278df483b326f3db45e
ojvl1 <carlosantompietri175@gmail.com>
2024-05-30 19:30:11 -0400

    fix the path

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 import sys
-sys.path.append('/Users/osvaldoantompietri/AirBnB_clone_v4/web_dynamic')
+sys.path.append('/Users/osvaldoantompietri/AirBnB_clone_v4')
 from models import storage
 from models.state import State
 from models.city import City
